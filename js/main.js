9c5736ca2020faaa2cc7b9614f70d91e01db3fbf
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-22 13:57:19 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+Palindroma
+Chiedere all'utente di inserire una parola
+Creare una funzione per invertire i caratteri di una stringa.
+Capire se la parola inserita è palindroma
+
+Pari e Dispari
+L'utente sceglie pari o dispari
+L'utente inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto in base alla scelta pario o dispari fatta all'inizio dall'utente.
+*/
